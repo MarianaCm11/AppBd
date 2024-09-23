d388cf0e45b23e6d384cdada929c8741751a282f
tuUsuarioEnGithub <tuCorreoEnGithub>
2024-09-23 07:45:59 -0600

    first cmmit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const usuariosRutas= require("./routes/rutasUsuario");
+const productosRutas= require("./routes/rutasProducto");
 
 const app= express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/", usuariosRutas);// el  json va antes de las rutas 
+app.use("/", productosRutas);
 
 const port= process.env.PORT || 3000;
 app.listen(port, ()=>{
--- routes/rutasUsuario.js
+++ routes/rutasUsuario.js
@@ -1,7 +1,5 @@
 var rutas = require("express").Router();
-// --var {Router}= require("express");
 var {mostrarUsuarios,nuevoUsu,borrarUsuario,buscarPorId}= require("../bd/usuariosBD");
-var {mostrarProductos,nuevoProducto,borrarProducto,buscarPorIdP}= require("../bd/productosBD");
 
 //USUARIOS
 rutas.get("/usuarios",async(req, res)=>{
@@ -25,27 +23,5 @@ rutas.post("/usuarios/nuevoUsuario",async (req, res)=>{
    var usuarioValido=await nuevoUsu(req.body);
    res.json(usuarioValido);
 });
-
-//PRODUCTO
-rutas.get("/productos",async(req, res)=>{
-   var productosValidos=await mostrarProductos();
-   res.json(productosValidos);
-});
-
-rutas.get("/productos/buscarPorIdP/:id", async(req,res)=>{
-   var productosValido =await buscarPorIdP(req.params.id);
-   res.json(productosValido);
-});
-
-rutas.delete("/productos/borrarProducto/:id", async(req,res)=>{
-   var productoBorrado=await borrarProducto (req.params.id);
-   res.json(productoBorrado);
-});
-
-rutas.post("/productos/nuevoProducto",async (req, res)=>{
-   var productoValido=await nuevoProducto(req.body);
-   res.json(productoValido);
-});
-
 
 module.exports=rutas;
